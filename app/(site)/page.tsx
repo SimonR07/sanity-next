2539b4fdf5cdf514ef4c7feb71b56e9090c9e7ae
Simon Rulquin <simon.rulquin@gmail.com>
2023-11-29 20:54:16 +0100

    add (site)+(studio)

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   return (
     <div>
       <h1 className='text-7xl text-center font-extrabold'><span className='bg-gradient-to-r from-orange-400 to-purple-600 bg-clip-text text-transparent'>The Darjeeling Limited</span>!</h1>
-      <p className='mt-8 text-xl text-gray-600'>The Whiteman family is a dysfunctional trio of estranged brothers on a spiritual journey through India, seeking to reconnect after their father's death.</p>
+      <p className='mt-8 text-xl text-gray-600'>The Whiteman family is a dysfunctional trio of estranged brothers on a spiritual journey through India, seeking to reconnect after their father&apos;s death.</p>
       <h2 className='mt-16 font-bold text-gray-700 text-3xl'>The Trio</h2>
       <div className='mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
         {projects.map(({name,_id,mainImage,slug})=>{
